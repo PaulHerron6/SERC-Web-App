26b9fb0a85fbe581f600f76a98fdf1f580562fac
Paul <paul6herron@hotmail.co.uk>
2022-07-08 15:36:04 +0100

    new88

--- tania/scripts.js
+++ tania/scripts.js
@@ -15,8 +15,8 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(dog => {
-      console.log(dog.message)
+    console.log(dog.message)
+    dog.forEach(dog => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
