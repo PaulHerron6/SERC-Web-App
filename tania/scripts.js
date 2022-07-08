d2fcbe3c2eca7f9e758a3c31f610799deb8601cb
Paul <paul6herron@hotmail.co.uk>
2022-07-08 14:08:01 +0100

    labrador

--- tania/scripts.js
+++ tania/scripts.js
@@ -10,21 +10,19 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/people', true)
+request.open('GET', 'https://dog.ceo/api/labrador/breeds/image/random', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(dog => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
-      const h1 = document.createElement('h1')
-      h1.textContent = movie.title
-
+    
       const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
+      dog.message = dog.message.substring(0, 300)
+      p.textContent = `${dog.message}...`
 
       container.appendChild(card)
       card.appendChild(h1)
